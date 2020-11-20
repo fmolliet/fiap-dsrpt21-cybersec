53beeb25fd9f15e0188799944ddf545bf32db671
fmolliet <fmolliet@hotmail.com>
2020-11-20 00:06:08 -0300

    Removido algumas dependencias não irá ser usada

--- server.py
+++ server.py
@@ -5,12 +5,10 @@ import importlib
 
 from PastebinCrawler       import PasteBin
 from HaveibeenpwnedCrawler import HaveIbeenPwed 
-from crawler import GoogleCrawler
-from pprint import pprint
+from crawler               import GoogleCrawler
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/search',  methods=["POST"])
 def search():
